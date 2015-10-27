cf60555bb650643ac25451780007ac5198605fdf
hobz_ <josh.n.hogg@gmail.com>
2015-10-27 17:11:30 +0000

    Cleanup Migrations Part 2

--- db/migrate/20151022033412_remove_created_at_and_updated_at_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCreatedAtAndUpdatedAtFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :created_at, :datetime
-    remove_column :users, :updated_at, :datetime
-  end
-end
--- db/migrate/20151022034138_add_user_id_and_name_and_nickname_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdAndNameAndNicknameToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :user_id, :integer
-    add_column :users, :name, :string
-    add_column :users, :nickname, :string
-  end
-end
